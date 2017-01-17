3cddf5fad8ce1d242ba9f175c12e2c3ef3099caf
philip putnam <putnamp989@gmail.com>
2017-01-16 19:33:17 -0800

    add loop for list output of user items in alphabetical order

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
 $(document).ready(function() {
   $("#grocery-list form").submit(function(event) {
+    $("#grocery-list").hide();
+
     var blanks = ["groceryItem1", "groceryItem2", "groceryItem3", "groceryItem4", "groceryItem5"];
     var userGroceries = [];
-
-    var stringTemp = "groceryItem";
 
     blanks.forEach(function(blank) {
       userGroceries.push($("input#"+ blank).val());
     });
 
-    alert(userGroceries);
+    var upperGroceries = userGroceries.map(function(string) {
+      return string.toUpperCase();
+    });
+
+    upperGroceries.sort();
+
+    // $(".groceryItem1").text(upperGroceries[0]);
+    // $(".groceryItem2").text(uppgerGroceries[1]);
+    var num = 0
+    blanks.forEach(function(blank) {
+        $("." + blank).append(upperGroceries[num]);
+        num += 1;
+    });
 
     event.preventDefault();
   });
